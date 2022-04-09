489577b71f4e5a4df0490c5c596d6c66a30d401a
Dhoniboy <103166082+Dhoniboy@users.noreply.github.com>
2022-04-09 09:37:37 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -15,17 +15,18 @@ async def start(c, m):
         )
     
     await m.reply_text(
-        text=f"Hi there {m.from_user.first_name}.\n\nI'm Screenshot Generator Bot. I can provide screenshots from your video files with out downloading the entire file (almost instantly). For more details check /help.",
+        text=f"Hi there {m.from_user.first_name}.\n\nI'm Screenshot Generator Bot. I can provide screenshots from your video files with out downloading the entire file (almost instantly)
+        OWNER :<a href=https://t.me/Dstamil_01>DS tamil</a> ❤️. For more details check /help.",
         quote=True,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('📌  Support Group', url='https://t.me/InFoTelGroup'),
-                    InlineKeyboardButton('🔖  Projects Channel', url='https://t.me/TGBotsZ')
+                    InlineKeyboardButton('📌  Support Group', url='https://t.me/+eN6ukkVwZNIzYzU1'),
+                    InlineKeyboardButton('🔖  Projects Channel', url='https://t.me/mm_dev_channel')
                 ],
                 [
-                    InlineKeyboardButton('💡  Source Code', url='https://github.com/TGExplore/Screenshot-Bot'),
-                    InlineKeyboardButton('👨  Master', url='https://t.me/odbots')
+                    InlineKeyboardButton('💡  Source Code', url='https://t.me/Dstamil_01'),
+                    InlineKeyboardButton('👨  Master', url='https://t.me/Dstamil_01')
                 ]
             ]
         )
